b85d5ab260640dbd0ff75e38b0d5176a5a26651a
sahan <sahangawesh19@gmail.com>
2024-11-06 02:13:13 +0530

    added delete flag

--- src/growth/growth.service.ts
+++ src/growth/growth.service.ts
@@ -4,8 +4,8 @@ import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class GrowthService {
-  
-  constructor(private readonly databaseService:DatabaseService){}
+
+  constructor(private readonly databaseService: DatabaseService) { }
 
   private async verifyParentChildRelation(parentId: number, childId: number) {
     const parentChildRelation = await this.databaseService.parentChild.findFirst({
@@ -47,26 +47,38 @@ export class GrowthService {
           gte: startDate,
           lte: endDate,
         },
+        isDeleted: false
       },
     });
   }
 
   async findAll(parentId: number, childId: number) {
-    await this.verifyParentChildRelation(parentId,childId)
+    await this.verifyParentChildRelation(parentId, childId)
     return this.databaseService.growth.findMany({
-      where:{
-        childId:childId
+      where: {
+        childId: childId,
+        isDeleted: false
       }
 
     });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} growth`;
+  async findOne(id: number, parentId: number) {
+    const record = await this.databaseService.growth.findUnique({
+      where: {
+        id,
+        isDeleted: false
+      }
+    })
+    if (!record) {
+      throw new NotFoundException("Growth Record Not Found");
+    }
+    await this.verifyParentChildRelation(parentId, record.childId);
+    return record;
   }
 
   async updateGrowthRecord(parentId: number, growthId: number, updateGrowthDto: Prisma.GrowthUpdateInput) {
-   
+
     const growthRecord = await this.databaseService.growth.findUnique({
       where: { id: growthId },
     });
@@ -76,7 +88,7 @@ export class GrowthService {
     }
 
     await this.verifyParentChildRelation(parentId, growthRecord.childId);
- 
+
     return this.databaseService.growth.update({
       where: { id: growthId },
       data: updateGrowthDto,
@@ -86,14 +98,18 @@ export class GrowthService {
   async deleteGrowthRecord(parentId: number, growthId: number) {
 
     const growthRecord = await this.databaseService.growth.findUnique({
-      where: { id: growthId },
+      where: {
+        id: growthId,
+        isDeleted: false
+      },
     });
     if (!growthRecord) {
       throw new NotFoundException(`Growth record with ID ${growthId} not found.`);
     }
     await this.verifyParentChildRelation(parentId, growthRecord.childId);
-    return this.databaseService.growth.delete({
+    return this.databaseService.growth.update({
       where: { id: growthId },
+      data: { isDeleted: true }
     });
   }
 }
