c1b407b4a186ec9b18046e01ac1ebd5cc91131e3
sahan <sahangawesh19@gmail.com>
2024-11-07 01:43:22 +0530

    Auth Endpoints according to specs

--- src/parent/dto/create-parent.request.ts
+++ src/parent/dto/create-parent.request.ts
@@ -1,20 +1,25 @@
 
-import { IsEmail, IsStrongPassword } from "class-validator";
+import { IsEmail, IsNotEmpty, IsStrongPassword } from "class-validator";
 
 
-export class CreateParentRequest{
-    
-  firstName :  String;
-  lastName  :  String;
+export class CreateParentRequest {
+
+  @IsNotEmpty()
+  firstName: string;
+  @IsNotEmpty()
+  lastName: string;
 
   @IsEmail()
-  email     :  String ;
-  
+  @IsNotEmpty()
+  email: string;
+
   @IsStrongPassword()
-  password   : String;
-  
-  image      : String;
-  bloodGroup  :String;
-  phoneNumber :String ; 
-  address    : String;
+  @IsNotEmpty()
+  password: string;
+
+  image: string;
+  bloodGroup: string;
+  @IsNotEmpty()
+  phoneNumber: string;
+  address: string;
 }
